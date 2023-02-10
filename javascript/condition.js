0849f433293813d74b01668bcd89f2d4053e1c42
koora1999 <122632056+koora1999@users.noreply.github.com>
2023-02-10 07:54:23 +0530

    Update condition.js

--- javascript/condition.js
+++ javascript/condition.js
@@ -12,6 +12,8 @@ function checkEvenorOdd()
     }
 }
 
+//assignment
+
 //wap whether the alpebet is vowel or consonent
 
 var alfha="o"
@@ -33,6 +35,37 @@ function VowelorConsonent(){
     }
 }
 VowelorConsonent();
+
+//wap display the week days by using the swicth condition
+//assignment
+
+function weekdays(){
+let week=6;
+switch(week){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednasday");
+        break;  
+        case 4:
+        console.log("thusday");
+        break;      
+        case 5:
+        console.log("friday");
+        break;      
+        case 6:
+        console.log("saturday");
+        break;      
+        case 7:
+        console.log("sunday");
+        break;            
+}
+}
+weekdays();
 
 //wap to display the largest number
 
@@ -74,32 +107,5 @@ function delivery(){
 let message2=delivery()
 order("ordered one dress on",message2);
 
-//wap display the week days by using the swicth condition
 
-function weekdays(){
-let week=6;
-switch(week){
-    case 1:
-        console.log("monday");
-        break;
-    case 2:
-        console.log("tuesday");
-        break;
-    case 3:
-        console.log("wednasday");
-        break;  
-        case 4:
-        console.log("thusday");
-        break;      
-        case 5:
-        console.log("friday");
-        break;      
-        case 6:
-        console.log("saturday");
-        break;      
-        case 7:
-        console.log("sunday");
-        break;            
-}
-}
-weekdays();
+
